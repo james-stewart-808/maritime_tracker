07c0b35efeb82b52d489989989a6c204f38e017e
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 09:16:02 +0100

    Update maritime_tracker.py

--- maritime_tracker.py
+++ maritime_tracker.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 # Define the pages
 main = st.Page("main.py", title="Home", icon="🏡")
-inventories = st.Page("inventories/inventories.py", title="Voyage-based Inventories", icon="🚢")
-trade = st.Page("trade/trade.py", title="Merchandise Trade Portfolios", icon="📦")
-impact_tracking = st.Page("impact_tracking/impact_tracking.py", title="Impact Tracking Results", icon="💵")
+#inventories = st.Page("inventories/inventories.py", title="Voyage-based Inventories", icon="🚢")
+#trade = st.Page("trade/trade.py", title="Merchandise Trade Portfolios", icon="📦")
+#impact_tracking = st.Page("impact_tracking/impact_tracking.py", title="Impact Tracking Results", icon="💵")
 #example_page = st.Page("streamlit_example.py", title="Streamlit Example", icon="❄️")
 
 
 # Set up navigation
-pg = st.navigation([main, inventories, trade, impact_tracking]) #, example_page])
+pg = st.navigation([main])#, inventories, trade, impact_tracking]) #, example_page])
 
 # Run the selected page
 pg.run()
