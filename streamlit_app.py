e535aa0b8d1d49fa0f4aa9c3bf9b1ec9d6600e25
james <61799526+james-stewart-808@users.noreply.github.com>
2026-03-30 09:47:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,26 @@
+from collections import defaultdict
+from pathlib import Path
+import sqlite3
+
+import streamlit as st
+import altair as alt
+import pandas as pd
+
+
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title="Inventory tracker",
+    page_icon=":shopping_bags:",  # This is an emoji shortcode. Could be a URL too.
+)
+
+
+
+
 import streamlit as st
 import pandas as pd
 
 # Define the pages
-main = st.Page("main.py", title="Home", icon="🏡")
+#main = st.Page("main.py", title="Home", icon="🏡")
 #inventories = st.Page("inventories/inventories.py", title="Voyage-based Inventories", icon="🚢")
 #trade = st.Page("trade/trade.py", title="Merchandise Trade Portfolios", icon="📦")
 #impact_tracking = st.Page("impact_tracking/impact_tracking.py", title="Impact Tracking Results", icon="💵")
@@ -10,7 +28,7 @@ main = st.Page("main.py", title="Home", icon="🏡")
 
 
 # Set up navigation
-pg = st.navigation([main])#, inventories, trade, impact_tracking]) #, example_page])
+#pg = st.navigation([main])#, inventories, trade, impact_tracking]) #, example_page])
 
 # Run the selected page
-pg.run()
+#pg.run()
